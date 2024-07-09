4ed4baa0f94c3263ec84165232c7b4c5464d52e5
neelakandan-01 <dev.neelakandan@gmail.com>
2024-07-09 16:50:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//DIGITAL CLOCK PROGRAM
+
+function  updateClock() {
+    const now=new Date();
+    let hours=now.getHours();
+    const meridiem=hours>12?"PM":"AM";
+    hours=hours%12||12;
+    hours.toString().padStart(2,0)
+    const minutes=now.getMinutes().toString().padStart(2,0);
+    const seconds=now.getSeconds().toString().padStart(2,0);
+
+    const timeString=`${hours}:${minutes}:${seconds}  ${meridiem}`;
+
+    let clock=document.getElementById("clock");
+    clock.innerHTML=timeString;
+}
+
+
+setInterval(updateClock,1000);
